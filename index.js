1451bfc105ffc1923112f6c30d7f28e660d509f9
mrise <mrise@mrise.local>
2021-11-11 21:39:34 -0300

    documentation added

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
+/**
+ * OBJETO CON PROPIEDADES COMO: ARREGLOS Y FUNCIONES
+ */
 const animales = {
+
+    /**
+     * Propiedad mascota, es un arreglo
+     */
     mascotas: ['perros','gatos','canarios','pez','serpiente','mariposa'],
+
+    /**
+     * La propiedad 'eliminarMascota' es una función anónima que recibe un parametro de tipo String,
+     * el método 'filter()' filtra el array y si los elementos del filter no son iguales al parámetro de la función,
+     * lo elimina y retorna el array mascotas actualizado con los elementos obtenidos del 'filter'
+     * @param {String}
+     */
     eliminarMascota: function(tipoMascota){
         const mascotasFilter = this.mascotas.filter(elemento => elemento !== tipoMascota)
         return this.mascotas = mascotasFilter
     },
+
+    /**
+     * La propiedad eliminarUltimaMascota es una función anónima que mediante el método 'pop()'elimina
+     * el último elemento del array mascotas y lo retorna actualizado 
+     */
     eliminarUltimaMacotas: function(){
         this.mascotas.pop()
         return this.mascotas
     },
+
+    /**
+     * La propiedad agregarMascotas es una función anónima que recibe un parámetro de tipo String y
+     * que mediante el método 'push()' actualiza el array mascotas y lo retorna con el nuevo elemento agregado
+     * @param {String} nuevaMascota 
+     */
     agregarMascotas: function(nuevaMascota){
         this.mascotas.push(nuevaMascota)
         return this.mascotas
     },
-    contadorMascotas: function(){
-        let mascotaConOs = 0
+
+    /**
+     * La propiedad contadorMascotasTerminaConOs es una función anónima que mediante el método 'forEach()'
+     * y el método 'endWith()' permite iterar y revisar si cada palabra que contiene el array termina con 'os'
+     * si el elemento cumple con la condición el contador 'mascotaTerminaConOs', inicializado en 0 
+     * le va sumando 1 y retornamos un String con un mensaje mostrando el número total 
+     * de coincidencias encontradas.
+     */
+    contadorMascotasTerminaConOs: function(){
+        let mascotaTerminaConOs = 0
         this.mascotas.forEach(elemento => {
             if(elemento.endsWith('os')){
-                mascotaConOs += 1
+                mascotaTerminaConOs += 1
             }  
         })
-        return `En el arreglo hay ${mascotaConOs} mascotas que terminan con 'os'`
+        return `En el arreglo hay ${mascotaTerminaConOs} mascotas que terminan con 'os'`
     }
 }
 
 
+/**
+ * USANDO DESESTRUCTURACIÓN
+ */
+
+
+/**
+ * variable de tipo Objecto
+ */
 const trabajador = {
     nombre: 'Jhon Smith',
     cargo: 'QA',
@@ -42,7 +83,14 @@ const trabajador = {
     hijos: null,
 }
 
-function mensaje(trabajador){
+/**
+ * La función mostrarMensaje desestructura el parámetro ingresado, hace una validación sobre la propiedad
+ * hijos: si la propiedad trae un 'undefined', se le asignará un valor de tipo String que lo reemplazará,
+ * y retorna String con un mensaje específico usando las variables de la desestructuración.
+ * @function mostrarMensaje
+ * @param {Object} trabajador 
+ */
+function mostrarMensaje(trabajador){
     const{
         nombre,
         cargo,
@@ -61,7 +109,13 @@ function mensaje(trabajador){
         le gusta ${gusto1}, ${resto.length} más y ${nuevoHijo}`
 }
 
-function mensaje2(trabajador){
+/**
+ * La función mostrarMensaje2 desestructura el parámetro ingresado, 
+ * y retorna un String con un mensaje específico usando las variables de la desestructuración.
+ * @function mostrarMensaje2
+ * @param {Object} trabajador 
+ */
+function mostrarMensaje2(trabajador){
     const{
         nombre,
         cargo,
